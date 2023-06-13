60cd30799594489166edbd626312ea0f251c83b1
gellison321 <gellison321@gmail.com>
2023-06-13 10:19:16 -0400

    optimize functions

--- tsshapelet/functions.py
+++ tsshapelet/functions.py
@@ -7,18 +7,26 @@ import peakutils
 ''' Array Manipulations '''
 
 def interpolate(array, length):
-    return interp1d(np.arange(0, len(array)), array)(np.linspace(0.0, len(array)-1, length))
+    if type(array) != np.array:
+        array = np.array(array)
+    array_length = len(array)
+    return interp1d(np.arange(0, array_length), array)(np.linspace(0.0, array_length-1, length))
 
 def reinterpolate(array, window_length):
-    array = list(array)
+    if type(array) != np.array:
+        array = np.array(array)
     length = len(array)
-    return np.array(array*(window_length//length)+array[:window_length%length])
+    return np.concatenate([np.tile(array, window_length//length),array[:window_length%length]])
 
 def pad(array, length):
+    if type(array) != np.array:
+        array = np.array(array)
     return np.pad(array, (0,length - len(array)), 'constant')
 
 def center_moving_average(array, period):
-    ret = np.cumsum(np.array(array))
+    if type(array) != np.array:
+        array = np.array(array)
+    ret = np.cumsum(array)
     ret[period:] = ret[period:] - ret[:-period]
     return ret[period - 1:] / period
 
@@ -28,11 +36,18 @@ def indexes(array, min_dist = 60, thres = 0.8):
 
 ''' Distance Metrics '''
 
-def cross_correlation(arr1, arr2):
-    return np.max(correlate(arr1, arr2))
+def cross_correlation(arr1, arr2, method = 'avg'):
+    if type(arr1) != np.array:
+        arr1 = np.array(arr1)
+        arr2 = np.array(arr2)
+    cases = {'avg': np.mean, 'max' : np.max, 'min' : np.min}
+    return cases[method](correlate(arr1, arr2))
 
 def euclidean_distance(arr1, arr2):
-    return np.sqrt(sum((np.array(arr1)-np.array(arr2))**2))
+    if type(arr1) != np.array:
+        arr1 = np.array(arr1)
+        arr2 = np.array(arr2)
+    return np.linalg.norm(arr1-arr2)
 
 def dynamic_time_warping(arr1, arr2):
     return dtw(arr1, arr2)
@@ -40,13 +55,17 @@ def dynamic_time_warping(arr1, arr2):
 
 ''' Barycenter Averaging '''
 
-def euclidean_barycenter(X):
-    return np.mean(np.array(X), axis = 0)
+def average_barycenter(X):
+    if type(X) != np.ndarray:
+        X = np.array(X)
+    return np.mean(X, axis = 0)
 
-def interpolated_barycenter(X):
+def interpolated_average(X):
+    if type(X) != np.array:
+        X = np.array(X, dtype = object)
     length = np.mean(np.array(list(map(len, X))), dtype = int)
     interpolated_candidates = [interpolate(arr, length) for arr in X]
-    return euclidean_barycenter(interpolated_candidates)
+    return average_barycenter(interpolated_candidates)
 
 
 ''' Scoping Maps '''
@@ -56,8 +75,8 @@ metrics  = {'euclidean' : euclidean_distance,
             'dtw' : dynamic_time_warping,
             }
 
-barycenters = {'interpolated' : interpolated_barycenter,
-               'euclidean' : euclidean_barycenter
+barycenters = {'interpolated' : interpolated_average,
+               'average' : average_barycenter
                }
 
 manipulations = {'interpolate' : interpolate,
